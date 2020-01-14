a7a7643d076e72afdd011b401f31e6eaa4641504
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-14 14:18:54 +0000

    files done, webpage displays

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("we're awesome");
+  new Vue({
+    el: '#app',
+    data: {
+
+    },
+    methods: {
+
+    }
+  });
+});
